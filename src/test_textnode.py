bc6f0eec646c94764105d4c09dbd67f6b20de4d8
RNahapeti <55406449+RNahapeti@users.noreply.github.com>
2026-04-15 17:03:59 -0700

    feat: add TextNode to HTMLNode converter and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+
 
 
 class TestTextNode(unittest.TestCase):
@@ -28,6 +29,33 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, url=None)
         node2 = TextNode("This is a text node", TextType.BOLD, url="https://theuselessweb.com/")
         self.assertNotEqual(node, node2)
+    
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text_to_html(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_style_to_html(self):
+        node = TextNode("This is a bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text")
+    
+    def test_link_to_html(self):
+        node = TextNode("Click me!", TextType.LINK, "https://theuselessweb.com/")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me!")
+        self.assertEqual(html_node.to_html(), '<a href="https://theuselessweb.com/">Click me!</a>')
+    
+    def test_img_to_html(self): # This is just to test the current simplified LeafNode setup... otherwise this is incorrect form for <img> tags
+        node = TextNode("Hip-hop-anonymous", TextType.IMAGE, "https://petsbynumbers.com/cdn/shop/articles/1_2_fca04971-c8f3-4cb7-aa10-aeefeb3d0722_1296x.webp")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.to_html(), '<img src="https://petsbynumbers.com/cdn/shop/articles/1_2_fca04971-c8f3-4cb7-aa10-aeefeb3d0722_1296x.webp" alt="Hip-hop-anonymous"></img>')
 
 
 if __name__ == "__main__":
