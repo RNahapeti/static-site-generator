11a8158c0814fc15efeef5e0a5c77639ccaf76f4
RNahapeti <55406449+RNahapeti@users.noreply.github.com>
2026-04-28 17:54:00 -0700

    feat: add block type detection for markdown

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,7 @@
+from enum import Enum
+
 from textnode import TextNode, TextType
+
 
 def markdown_to_blocks(markdown):
     final_blocks = []
@@ -7,3 +10,30 @@ def markdown_to_blocks(markdown):
         if block:
             final_blocks.append(block.strip())
     return final_blocks
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+def block_to_block_type(md_block):
+    headings = ["# ", "## ", "### ", "#### ", "##### ", "###### "]
+
+    if any(md_block.startswith(heading) for heading in headings):
+        return BlockType.HEADING
+    if md_block.startswith("```\n") and md_block.endswith("```"):
+        return BlockType.CODE
+    
+    lines = md_block.split("\n")
+
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+    if all(line.startswith(f"{i}. ") for i, line in enumerate(lines, start=1)):
+        return BlockType.ORDERED_LIST
+    # if none of the above catch a blocktype then it must be...
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -42,6 +42,84 @@ This is the same paragraph on a new line
             "- This is a list\n- with items",
         ]
         self.assertListEqual(expected, blocks)
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_simple_paragraph(self):
+        block = "This is just a normal paragraph block with some text."
+        self.assertEqual(BlockType.PARAGRAPH, block_to_block_type(block))
+    
+    def test_complex_paragraph(self):
+        block = "\n".join([
+            "> we start off with a quote",
+            "but then end with a standard paragraph",
+            "1. lets throw in one ordered list item as well",
+        ])
+        self.assertEqual(BlockType.PARAGRAPH, block_to_block_type(block))
+    
+    def test_headings(self):
+        block = "###### Who uses headings this deep?"
+        self.assertEqual(BlockType.HEADING, block_to_block_type(block))
+
+    def test_quote(self):
+        block = "> this happens to be a quote"
+        self.assertEqual(BlockType.QUOTE, block_to_block_type(block))
+
+    def test_quote_multi(self):
+        block = "\n".join([
+            "> Let's start on a quote",
+            ">This one has no space after",
+            "> But this one does again!"
+        ])
+        self.assertEqual(BlockType.QUOTE, block_to_block_type(block))
+    
+    def test_unordered_list(self):
+        block = "\n".join([
+            "- list item 1",
+            "- another list item",
+            "- we're unordered",
+        ])
+        self.assertEqual(BlockType.UNORDERED_LIST, block_to_block_type(block))
+
+    def test_unordered_mistake(self):
+        block = "\n".join([
+            "- start off with unordered list",
+            "-whoops we forgot that space, what'll happen?",
+        ])
+        self.assertEqual(BlockType.PARAGRAPH, block_to_block_type(block))
+    
+    def test_ordered_list(self):
+        block = "\n".join([
+            "1. first",
+            "2. second",
+            "3. third",
+        ])
+        self.assertEqual(BlockType.ORDERED_LIST, block_to_block_type(block))
+
+    def test_ordered_mistake(self):
+        block = "\n".join([
+            "1. first",
+            "2. second",
+            "4. my bad forgot third",
+        ])
+        self.assertEqual(BlockType.PARAGRAPH, block_to_block_type(block))
+
+    def test_code_block(self):
+        block = "\n".join([
+            "```",
+            "we got us some code here",
+            "```",
+        ])
+        self.assertEqual(BlockType.CODE, block_to_block_type(block))
+
+    def test_code_mistake(self):
+        block = "\n".join([
+            "```",
+            "some code again",
+            "we fogot the closing tag",
+        ])
+        self.assertEqual(BlockType.PARAGRAPH, block_to_block_type(block))
+
 
 if __name__ == "__main__":
     unittest.main()
