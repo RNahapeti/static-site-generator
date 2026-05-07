dd4af298b56cd3dfe157668b86bc1d579a65410b
RNahapeti <55406449+RNahapeti@users.noreply.github.com>
2026-05-06 18:10:45 -0700

    feat: add recursive page generation
    Crawls the content directory tree and generates an HTML page for each
    markdown file, mirroring the directory structure in public/.

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+from pathlib import Path
 
 from block_markdown import markdown_to_html_node
 
@@ -27,3 +29,15 @@ def generate_page(from_path, template_path, dest_path):
 
     with open(dest_path, "w") as f:
         f.write(final_html)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for file in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, file)
+        dest_path = os.path.join(dest_dir_path, file)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
+
+    
