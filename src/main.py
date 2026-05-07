7e20a5f7156ffaaa9a26bb3ba02698416c259c76
RNahapeti <55406449+RNahapeti@users.noreply.github.com>
2026-05-06 17:48:44 -0700

    chore: add blog and contact content pages + images

--- src/main.py
+++ src/main.py
@@ -4,4 +4,8 @@ from gencontent import generate_page
 def main():
     setup_public_dir("./static", "./public")
     generate_page("./content/index.md", "./template.html", "public/index.html")
+ #   generate_page("./content/contact/index.md", "./template.html", "public/contact/index.html")
+ #   generate_page("./content/blog/glorfindel/index.md", "./template.html", "public/blog/glorfindel/index.html")
+ #   generate_page("./content/blog/majesty/index.md", "./template.html", "public/blog/majesty/index.html")
+ #   generate_page("./content/blog/tom/index.md", "./template.html", "public/blog/tom/index.html")
 main()
